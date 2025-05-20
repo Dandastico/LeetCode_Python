a40dfeeb14c3c1a06c05c4f1ed1d75fad28207ec
Dandastico <dandastico.bsb@gmail.com>
2025-05-20 14:39:52 -0300

    ALTERAÇÃO: criei função própria que funciona como str.find()

--- easy/0028_find-the-index-of-the-first-occurrence-of-a-string.py
+++ easy/0028_find-the-index-of-the-first-occurrence-of-a-string.py
@@ -16,3 +16,32 @@ Explanation: "leeto" did not occur in "leetcode", so we return -1.
 # tem um método em Python que se chama str.find() que faz isso tudo
 def strStr(haystack, needle):
     return haystack.find(needle)
+
+
+# vou tentar fazer uma função que faz o que o str.find() faz
+def find_idx(haystack, needle):
+    length = len(needle)
+    err = -1
+    
+    if length < 1 or length > len(haystack):
+        return err
+    
+    if length == len(haystack) and needle != haystack:
+        return err
+    
+    for i in range(len(haystack)):
+        if (length + i) > (len(haystack) + 1):
+            return err
+
+        if (length + i) == len(haystack):
+            if needle == haystack[i:]:
+                return i
+
+        if needle == haystack[i:i+length]:
+            return i
+        
+    return err
+
+
+print(find_idx("sadbutsad", "sad"))
+print(find_idx("leetcode", "leeto"))
